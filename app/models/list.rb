f85712043a0940fb9a083112d331e2ed6518db7c
EC2 Default User <ec2-user@ip-172-31-4-136.ec2.internal>
2021-11-22 02:07:44 +0000

    add new file

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,8 @@
 class List < ApplicationRecord
+  attachment :image
+
+　validates :title, presence: true
+  validates :body, presence: true
+  validates :image, presence: true
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '97daf6e7ed8e770f034146e14c282ca435eaae402320c05b06893e311918cecf60db675827f5d79d31f646574a93b5c02de88aff2842b52c3dcc7c2241eae439'
